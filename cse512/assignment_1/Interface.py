7e17a1658ff6fe86fddaf32b7283caf26e5e4241
Andrew Dudley <addudley@asu.edu>
2018-09-13 18:30:49 -0700

    finished cse512 hw1 assignment

--- cse512/assignment_1/Interface.py
+++ cse512/assignment_1/Interface.py
@@ -5,6 +5,7 @@ DATABASE_NAME = 'dds_assgn1'
 RANGE_TABLE_PREFIX = 'range_part'
 RROBIN_TABLE_PREFIX = 'rrobin_part'
 RROBIN_METADATA_TABLE = 'rrobin_metadata'
+RANGE_METADATA_TABLE = 'range_metadata'
 
 def getopenconnection(user='postgres', password='1234', dbname='postgres'):
     return psycopg2.connect("dbname='" + dbname + "' user='" + user + "' host='localhost' password='" + password + "'")
@@ -22,11 +23,12 @@ def rangepartition(ratingstablename, numberofpartitions, openconnection):
     cur = openconnection.cursor()
     increment = 5 / numberofpartitions
     lower_limit = -1
+    cur.execute("CREATE TABLE IF NOT EXISTS {} (name VARCHAR(40) NOT NULL, partition INTEGER, lower FLOAT, upper FLOAT, PRIMARY KEY (name, partition));".format(RANGE_METADATA_TABLE))
     for partition in range(numberofpartitions):
         createTableString = "CREATE TABLE {}{} (LIKE {} INCLUDING ALL);".format(RANGE_TABLE_PREFIX, partition, ratingstablename)
-        print(createTableString)
         cur.execute(createTableString)
         cur.execute("INSERT INTO {}{} SELECT * FROM {} WHERE rating > {} AND RATING <= {}".format(RANGE_TABLE_PREFIX, partition, ratingstablename, lower_limit, (partition + 1)*increment))
+        cur.execute("INSERT INTO {} VALUES ('{}', {}, {}, {});".format(RANGE_METADATA_TABLE, ratingstablename, partition, lower_limit, (partition + 1) * increment))
         lower_limit = (partition + 1) * increment
     cur.close()
 
@@ -66,10 +68,18 @@ def roundrobininsert(ratingstablename, userid, itemid, rating, openconnection):
                 BEGIN;
                 INSERT INTO {} VALUES ({}, {}, {}, {});
                 UPDATE {} SET insert_idx = insert_idx + 1
-                WHERE name = {};
+                WHERE name = '{}';
                 COMMIT;"""
-                .format(RROBIN_TABLE_PREFIX + str(insert_idx), userid, itemid, rating, current_time, 
-                    RROBIN_METADATA_TABLE, ratingstablename))
+                .format(RROBIN_TABLE_PREFIX + str(insert_idx), userid, itemid, rating, current_time,
+                        RROBIN_METADATA_TABLE, ratingstablename))
 
-
-
+def rangeinsert(ratingstablename, userid, itemid, rating, openconnection):
+    cur = openconnection.cursor()
+    cur.execute("SELECT name, partition, lower, upper from {} WHERE name = '{}' AND lower < {} AND upper >= {};".format(RANGE_METADATA_TABLE, ratingstablename, rating, rating))
+    result = cur.fetchone()
+    if not result:
+        print "ERROR: No range partition found appropriate for the rating provided!"
+        return
+    partition = result[1]
+    current_time = int( time.time() )
+    cur.execute("INSERT INTO {} VALUES ({}, {}, {}, {})".format(RANGE_TABLE_PREFIX + str(partition), userid, itemid, rating, current_time))
